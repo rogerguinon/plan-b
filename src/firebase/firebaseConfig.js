82d6a16a555a21e6ca78e2f6eb59d98998bebde2
Roger <roger.guinon@gmail.com>
2025-05-17 19:36:37 +0200

    stored Firebase keys/secrets in .env

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,14 +1,15 @@
 // src/firebase/firebaseConfig.js
 import { initializeApp, getApps } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import Config from 'react-native-config';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDBkmx6zsuGmN6aR_bf0JC91gwQvVDYFcE",
-  authDomain: "plan-b-d83b2.firebaseapp.com",
-  projectId: "plan-b-d83b2",
-  storageBucket: "plan-b-d83b2.appspot.com",
-  messagingSenderId: "435441365762",
-  appId: "1:435441365762:web:e167886875515af3bc481b"
+  apiKey: Config.FIREBASE_API_KEY,
+  authDomain: Config.FIREBASE_AUTH_DOMAIN,
+  projectId: Config.FIREBASE_PROJECT_ID,
+  storageBucket: Config.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: Config.FIREBASE_MESSAGING_SENDER_ID,
+  appId: Config.FIREBASE_APP_ID,
 };
 
 // Esto previene múltiples inicializaciones
