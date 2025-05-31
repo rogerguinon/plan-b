295ef3d8bc9ddeb982ff3e64497f0dc7aa398c3e
rogerguinon <roger.guinon01@estudiant.upf.edu>
2025-05-31 16:18:46 +0200

    change convertirFecha function to make it compatible with web

--- src/screens/CalendarScreen/CalendarScreen.js
+++ src/screens/CalendarScreen/CalendarScreen.js
@@ -6,14 +6,16 @@ import AntDesign from '@expo/vector-icons/AntDesign';
 import { useEventos } from '../../context/EventContext';
 
 const convertirFecha = (fechaStr) => {
+  const fecha = new Date(fechaStr);
+  if (isNaN(fecha)) return '';
 
-  const meses = {
-    Jan: '01', Feb: '02', Mar: '03', Apr: '04', May: '05', Jun: '06',
-    Jul: '07', Aug: '08', Sep: '09', Oct: '10', Nov: '11', Dec: '12'
-  };
-  const [mes, dia, anio] = fechaStr.replace(',', '').split(' ');
-  return `${anio}-${meses[mes]}-${dia.padStart(2, '0')}`;
+  const year = fecha.getFullYear();
+  const month = String(fecha.getMonth() + 1).padStart(2, '0');
+  const day = String(fecha.getDate()).padStart(2, '0');
+
+  return `${year}-${month}-${day}`;
 };
+
 
 
 LocaleConfig.locales['es'] = {
